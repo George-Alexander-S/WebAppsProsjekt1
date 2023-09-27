1d2f0b96db886ac6afcc07bd8cf54aa30c8548e0
Gromfader <galex.saa@hotmail.com>
2023-09-27 12:54:51 +0200

    Made a FlashCardController and the start of the CreateCard functionality

---
+++ WebAppsProsjekt1/Views/FlashCard/CreateCard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppsProsjekt1.Views.FlashCard;
+
+public class CreateCard : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
