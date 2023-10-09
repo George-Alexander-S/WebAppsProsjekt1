2ce7517e4f42c49694f94e09832e55760178ab79
Gromfader <galex.saa@hotmail.com>
2023-10-09 15:42:19 +0200

    Some changes to createcard and its css, and trying to add backend for displaying cards as well

--- WebAppsProsjekt1/Controllers/FlashCardController.cs
+++ WebAppsProsjekt1/Controllers/FlashCardController.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using WebAppsProsjekt1.Models;
 
 namespace WebAppsProsjekt1.Controllers;
 
 public class FlashCardController : Controller
 {
+
+    public IActionResult FlashCardTable()
+    {
+        var flashcards = new List<FlashCard>();
+        var flashcard1 = new FlashCard(1, "Denne teksten er foran", "Denne teksten er bak");
+        
+        flashcards.Add(flashcard1);
+
+        ViewBag.CurrentViewName = "Flashcards";
+        return View(flashcards);
+    }
+    
     public IActionResult CreateCard()
     {
         return View();
