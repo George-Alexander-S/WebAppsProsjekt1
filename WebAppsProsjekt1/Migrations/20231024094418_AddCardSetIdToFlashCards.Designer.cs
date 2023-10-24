82dad5d2171b7d00d244ebd12f24836e5a707932
Gromfader <galex.saa@hotmail.com>
2023-10-24 12:01:28 +0200

    More migrations

--- WebAppsProsjekt1/Migrations/20231024094418_AddCardSetIdToFlashCards.Designer.cs
+++ WebAppsProsjekt1/Migrations/20231024094418_AddCardSetIdToFlashCards.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppsProsjekt1.Models;
 
@@ -10,9 +11,11 @@ using WebAppsProsjekt1.Models;
 namespace WebAppsProsjekt1.Migrations
 {
     [DbContext(typeof(CardDbContext))]
-    partial class CardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024094418_AddCardSetIdToFlashCards")]
+    partial class AddCardSetIdToFlashCards
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
